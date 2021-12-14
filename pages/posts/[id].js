85f8e25fbdcf7c45299ea54f07f74fcce11b5dae
Felicia Ebikon <feliciaebikon@gmail.com>
2021-12-14 04:05:18 +0100

    pre-rendering and data fetching

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Layout from '../../components/layout'
-
+import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-
+import Date from '../../components/date'
 export async function getStaticProps({ params }) {
     const postData = await getPostData(params.id)
     return {
@@ -22,11 +22,15 @@ export async function getStaticPaths() {
 export default function Post({ postData }) {
     return (
       <Layout>
+
+        <Head>
+            <title>{postData.title}</title>
+        </Head>
         {postData.title}
         <br />
         {postData.id}
         <br />
-        {postData.date}
+        <Date dateString={postData.date} />
         <br />
         <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
       </Layout>
